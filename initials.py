0fb38f552c5451d7843bcd60b9c1fe6e7cb11cc8
MichaelAllan-David <2425403673@live.gctu.edu.gh>
2026-05-30 23:52:12 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,6 +1,31 @@
 
 def main():
   pass #code goes here
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+#
+# Name: Michael Nii Akwei ALlan-David
+# STUDENT ID: 2425403673
+# Email: 2425403673@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Michael Nii Akwei ALlan-David, this programming assignment is my own work
+# and I have not provided this code to any other student.
+print ("")
+print ("...MNAAD")
+print ("")
+print ("...MMMM....MMMM........NNN.......NN.........AAAAAAAAAA..........AAAAAAAAAA.........DDDDDDDD.........")
+print ("...MMMM....MMMM........NNNN......NN........AAAAAAAAAAAA........AAAAAAAAAAAA........DDDDDDDDD........")
+print ("...MM.MM..MM.MM........NN.NN.....NN........AAA......AAA........AAA......AAA........DD......DDD......")
+print ("...MM.MM..MM.MM........NN..NN....NN........AAA......AAA........AAA......AAA........DD........DD.....")
+print ("...MM.MM..MM.MM........NN...NN...NN........AAA......AAA........AAA......AAA........DD........DD.....")
+print ("...MM..MMMM..MM........NN....NN..NN........AAAAAAAAAAAA........AAAAAAAAAAAA........DD........DD.....")
+print ("...MM..MMMM..MM........NN.....NN.NN........AAAAAAAAAAAA........AAAAAAAAAAAA........DD........DD.....")
+print ("...MM........MM........NN......N.NN........AAA......AAA........AAA......AAA........DD.......DDD.....")
+print ("...MM........MM...**...NN.......NNN...**...AAA......AAA...**...AAA......AAA...**...DDDDDDDDD......**")
+print ("...MM........MM...**...NN.......NNN...**...AAA......AAA...**...AAA......AAA...**...DDDDDDDD.......**")
+print ("")
 
 
 
